2d81a24adc6ee39fee82a9e7cce915f576616eda
enzomaneira <enzo@maneira.com>
2024-04-11 20:58:11 -0300

    adding logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
-
+import mmbLogo from "../../public/mmb.png";
 
 function Navbar() {
     return (
     <nav className={styles.navbar}>
+        <div className={styles.logoContainer}>
+            <img src={mmbLogo} alt="MMB Logo" className={styles.logo} />
+        </div>
         <div className={styles.right}>
         <h2><Link to="/registrarCliente">Registrar</Link>
         <Link to="/buscarCliente">Buscar</Link>
